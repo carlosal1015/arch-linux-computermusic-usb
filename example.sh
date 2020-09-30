851683cdb564b5d1a2a7ed67a9bd1309cb7c701d
mads kjeldgaard <mail@madskjeldgaard.dk>
2020-09-30 09:24:55 +0200

    wma

--- example.sh
+++ example.sh
@@ -1,1 +1,1 @@
-ALMA_USER="mads" TIMEZONE="Europe/Oslo" ALMA_LOCALE="en_UK.UTF-8" ALMA_KB_LAYOUT="dk" sudo -E alma create --presets preset
+ALMA_USER="mads" TIMEZONE="Europe/Oslo" ALMA_LOCALE="en_GB.UTF-8" ALMA_KB_LAYOUT="dk" sudo -E alma create --presets preset
